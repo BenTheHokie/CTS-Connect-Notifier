aba0941a850544d957cd90fe8e20ba55c26027ff
lemonsourkid <lemonsourkid@gmail.com>
2012-10-07 12:42:05 -0400

    First commit
    It's a "prototype".

---
+++ notifier.js
@@ -0,0 +1,15 @@
+djId = '';
+
+function djs() {
+    for (var i in turntable) {
+        if (turntable[i].djIds) {
+            return turntable[i].djIds;
+        }
+    }
+}
+turntable.addEventListener('message', function (m) {
+    if (m.command == 'newsong') {
+        djId = m.room.metadata.current_dj;
+    }
+  if (m.command == 'newsong'||m.command=='rem_dj') {if (djs().indexOf(turntable.user.id)==djs().indexOf(djId)+1){alert("It's your turn to connect!");}}
+});

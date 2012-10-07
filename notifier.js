b0c3e356b3e2c7cdc05f7a0635380c0ef737ea48
lemonsourkid <lemonsourkid@gmail.com>
2012-10-07 13:45:23 -0300

    Prettyify

--- notifier.js
+++ notifier.js
@@ -11,5 +11,9 @@ turntable.addEventListener('message', function (m) {
     if (m.command == 'newsong') {
         djId = m.room.metadata.current_dj;
     }
-  if (m.command == 'newsong'||m.command=='rem_dj') {if (djs().indexOf(turntable.user.id)==djs().indexOf(djId)+1){alert("It's your turn to connect!");}}
+    if (m.command == 'newsong' || m.command == 'rem_dj') {
+        if (djs().indexOf(turntable.user.id) == djs().indexOf(djId) + 1) {
+            alert("It's your turn to connect!");
+        }
+    }
 });
